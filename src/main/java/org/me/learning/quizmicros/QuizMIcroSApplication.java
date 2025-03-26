765af5ac58528292bbd4f1c0b51f10bc98c5bd31
mohamadalabsi <absimoda1@gmail.com>
2025-03-26 23:56:11 +0100

    first step into quiz service

---
+++ src/main/java/org/me/learning/quizmicros/QuizMIcroSApplication.java
@@ -0,0 +1,13 @@
+package org.me.learning.quizmicros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizMIcroSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizMIcroSApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/me/learning/quizmicros/QuizMIcroSApplicationTests.java
@@ -0,0 +1,13 @@
+package org.me.learning.quizmicros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizMIcroSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
